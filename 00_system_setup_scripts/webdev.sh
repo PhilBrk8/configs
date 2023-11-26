325346607c39584126ba846cb65ac5e4363b72eb
Phil Brk8 <philbrk8@gmail.com>
2023-11-26 13:12:43 +0100

    added nodejs npm and pnpm installation

---
+++ 00_system_setup_scripts/webdev.sh
@@ -0,0 +1,12 @@
+dnf module list nodejs #list available streams for:
+sudo dnf module install nodejs:
+#OOOR:
+sudo dnf install npm -y
+
+# Turso - Database
+# curl -sSfL https://get.tur.so/install.sh | bash
+# turso db create # create a DB from the CLI
+
+# install pnpm - performance npm
+npm install -g pnpm
+
